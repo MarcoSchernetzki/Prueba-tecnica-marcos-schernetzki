060759c0eacae5c7b00295fbbdae93beb6d02632
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-19 20:29:33 +0100

    Update method post

--- src/infrasctructure/components/routes/app.routes.jsx
+++ src/infrasctructure/components/routes/app.routes.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import CreatePage from "../../../features/pages/createPage/createPage";
 import { HomePage } from "../../../features/pages/homePage/homePage";
 
 export function AppRoutes() {
     return (
         <Routes>
-            <Route path="Añadir" element={<HomePage />}></Route>
+            <Route path="Añadir" element={<CreatePage />}></Route>
             <Route path="" element={<HomePage />}></Route>
             <Route path="*" element={<HomePage />}></Route>
         </Routes>
