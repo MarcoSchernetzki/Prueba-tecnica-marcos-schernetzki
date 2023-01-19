6830c41d9663021f897ddbc62a8686ae244a4ee5
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-19 20:37:54 +0100

    Add useSite test

--- src/features/site/hook/useSite.js
+++ src/features/site/hook/useSite.js
@@ -2,6 +2,7 @@ import { useCallback, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { SiteRepository } from "../service/siteRepository";
 import * as ac from "../reducer/action.creator";
+import Swal from "sweetalert2";
 
 export const useSite = () => {
     const sites = useSelector((state) => state.sites);
@@ -15,7 +16,9 @@ export const useSite = () => {
                 dispatcher(ac.loadActionSite(sites));
             })
 
-            .catch((error) => console.log(error.name, error.message));
+            .catch((error) => {
+                return Swal.fire("Error", error.message, "error");
+            });
     }, [apiSite, dispatcher]);
 
     const handleAdd = (newSite) => {
