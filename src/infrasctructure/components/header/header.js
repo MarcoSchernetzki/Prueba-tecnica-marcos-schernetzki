3643c657167d838bd444a42bdcaa0f7f38bfdcec
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-19 11:42:01 +0100

    Add component Menu

--- src/infrasctructure/components/header/header.js
+++ src/infrasctructure/components/header/header.js
@@ -1,7 +1,12 @@
+import { Menu } from "../menu/menu";
+
 export function Header() {
     return (
         <header>
             <h1>A3PLAYER</h1>
+            <div>
+                <Menu />
+            </div>
         </header>
     );
 }
