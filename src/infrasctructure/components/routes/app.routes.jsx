6cb6f9f760ebf0b162a41583ab0251a3e0c1a7f2
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-19 12:26:56 +0100

    Add component AppRoutes

---
+++ src/infrasctructure/components/routes/app.routes.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+export function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="Añadir" element={<p>Proximamente</p>}></Route>
+            <Route path="" element={<p>Proximamente</p>}></Route>
+
+            <Route path="*" element={<p>Proximamente</p>}></Route>
+        </Routes>
+    );
+}
