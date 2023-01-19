a85a78ebda03e7d96e6e43dbe3069eea215465d0
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-19 11:06:48 +0100

    Update test component App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { App } from "./App";
 
-test("renders learn react link", () => {
+test("renders proximamente", () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText(/proximamente/i);
     expect(linkElement).toBeInTheDocument();
 });
