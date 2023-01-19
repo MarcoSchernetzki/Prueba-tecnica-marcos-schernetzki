ccfe90e86da2f212e2afe8ccf3476f93935bbb63
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-19 13:14:40 +0100

    Add store and update test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import { App } from "./App";
+import { appStore } from "./infrasctructure/store/store";
 
 test("renders proximamente", () => {
     render(
         <Router>
-            <App />
+            <Provider store={appStore}>
+                <App />
+            </Provider>
         </Router>
     );
     const linkElement = screen.getByText(/proximamente/i);
