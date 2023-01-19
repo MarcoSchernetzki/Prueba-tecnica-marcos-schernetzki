ae600c20f3880bc931f7836c5d70843a6d27d5cd
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-19 20:27:20 +0100

    Add create page

---
+++ src/features/pages/createPage/createPage.jsx
@@ -0,0 +1,16 @@
+import { CreateForm } from "./createForm/createForm";
+
+function CreatePage() {
+    return (
+        <>
+            <main>
+                <h2>Añade un sitio</h2>
+                <div>
+                    <CreateForm />
+                </div>
+            </main>
+        </>
+    );
+}
+export default CreatePage;
+
