043eb6375a6b3949288130a4af686ea313862a76
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-20 01:57:35 +0100

    Add update page

---
+++ src/features/pages/updatePage/updatePage.jsx
@@ -0,0 +1,12 @@
+import { UpdateForm } from "./updateForm/updateForm";
+
+export function UpdatePage() {
+    return (
+        <>
+            <main>
+                <h2>Edita tu sitio</h2>
+                <UpdateForm />
+            </main>
+        </>
+    );
+}
