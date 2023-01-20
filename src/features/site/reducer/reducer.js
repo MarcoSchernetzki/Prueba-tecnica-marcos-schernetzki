856be08f53bfc0c43abc369fdd7f2227b17bdb2d
marcos schernetzki <marcoschernetzki@gmail.com>
2023-01-20 01:57:08 +0100

    fix test update reducer

--- src/features/site/reducer/reducer.js
+++ src/features/site/reducer/reducer.js
@@ -12,19 +12,21 @@ export const siteReducer = createReducer(initialState, (builder) => {
         ...state,
         sites: [...state.sites, action.payload],
     }));
-    builder.addCase(ac.updateActionSite, (state, action) => ({
-        ...state,
-        sites: state.sites.map((item) =>
-            item.id === action.payload.id ? action.payload : item
-        ),
-    }));
+    builder.addCase(ac.updateActionSite, (state, action) => {
+        return {
+            ...state,
+            sites: state.sites.map((item) =>
+                item._id === action.payload._id ? action.payload : item
+            ),
+        };
+    });
     builder.addCase(ac.selectActionCreator, (state, action) => ({
         ...state,
         selectedSite: action.payload,
     }));
     builder.addCase(ac.deleteActionSite, (state, action) => ({
         ...state,
-        sites: state.sites.filter((item) => item.id !== action.payload),
+        sites: state.sites.filter((item) => item._id !== action.payload),
     }));
     builder.addDefaultCase((state) => state);
 });
